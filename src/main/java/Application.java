8d528e2d0e3d736132434564104a2343c3ad11ca
maxim.yenalyev <maxim.yenalyev@gmail.com>
2021-12-20 22:21:07 +0200

    Initial commit

---
+++ src/main/java/Application.java
@@ -0,0 +1,13 @@
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+
+
+//@SpringBootApplication
+@EnableAutoConfiguration
+public class Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
